530e1656c9300a4c29f4196891cc15d8a08212c4
Cyril Clement <dossiman@gmail.com>
2018-12-13 04:10:32 +0700

    add docstring and unittest for most methods

--- pyost/iost.py
+++ pyost/iost.py
@@ -8,7 +8,18 @@ import protobuf_to_dict as ptd
 
 
 class IOST():
-    def __init__(self, url, timeout=10):
+    """
+    This class provides API access to the IOST blockchain.
+    """
+
+    def __init__(self, url: str, timeout: int = 10):
+        """
+        Connects to a node.
+
+        Args:
+            url (str): Node's IP address and port number.
+            timeout (int): Number of seconds to wait when querying the node until timing out.
+        """
         self._channel = grpc.insecure_channel(url)
         self._stub = None
         self._timeout = timeout
@@ -19,8 +30,7 @@ class IOST():
         else:
             self._stub = apis_pb2_grpc.ApisStub(self._channel)
 
-    # TODO: raise error if params don't match the type of protos
-    # TODO: by typing params or using assert?
+    # // get the current height of the blockchain
     # rpc GetHeight (google.protobuf.Empty) returns (HeightRes) {
     #    option (google.api.http) = {
     #        get: "/getHeight"
@@ -30,7 +40,16 @@ class IOST():
     # 	// the height of the blockchain
     # 	int64 height=1;
     # }
-    def get_height(self):
+    def get_height(self) -> int:
+        """
+        Gets the current height of the blockchain.
+
+        Note:
+            REST API: "/getHeight"
+
+        Returns:
+            The height of the blockchain.
+        """
         res = self._stub.GetHeight(Empty())
         return res.height
 
@@ -68,9 +87,46 @@ class IOST():
     #     bytes sig = 2;
     #     bytes pubKey = 3;
     # }
-    def get_tx_by_hash(self, tx_hash):
+    def get_tx_by_hash(self, tx_hash: str) -> (dict, bytes):
+        """
+        Gets a transaction by its hash value.
+
+        Note:
+            REST API: "/getTxByHash/{hash}"
+
+        Args:
+            tx_hash (str): The base58 hash string of the transaction.
+
+        Returns:
+            (dict, bytes): A tuple containing the transaction content as a dict
+                and its hash as bytes. The dict has the following format::
+
+                {
+                    'time': int,
+                    'expiration': int,
+                    'gasLimit': int,
+                    'gasPrice': 1,
+                    'actions': [{
+                        'contract': string,
+                        'actionName': string,
+                        'data': string
+                    }],
+                    'signers': bytes,
+                    'signs': [{
+                        'algorithm': int,
+                        'sig': bytes,
+                        'pubKey':  bytes
+                    }],
+                    'publisher': {
+                        'algorithm': int,
+                        'sig': bytes,
+                        'pubKey': bytes
+                    }
+                }
+        """
         req = apis_pb2.HashReq(hash=tx_hash)
         res = self._stub.GetTxByHash(req)
+        print(ptd.get_field_names_and_options(res))
         return (protobuf_to_dict(res.txRaw), res.hash)
 
     # // get receipt by hash
@@ -101,7 +157,34 @@ class IOST():
     #     int32 type = 1;
     #     string content = 2;
     # }
-    def get_tx_receipt_by_hash(self, receipt_hash):
+    def get_tx_receipt_by_hash(self, receipt_hash: str) -> (dict, bytes):
+        """
+        Gets a transaction receipt by its receipt hash value.
+
+        Note:
+            REST API: "/getTxReceiptByHash/{hash}"
+
+        Args:
+            receipt_hash (str): The base58 hash string of the transaction receipt.
+
+        Returns:
+            (dict, bytes): A tuple containing the receipt content as a dict
+                and its hash as bytes. The dict has the following format::
+
+                {
+                    'txHash': bytes,
+                    'gasUsage': int,
+                    'status': {
+                        'code': int,
+                        'message': string
+                    },
+                    'succActionNum': int,
+                    'receipts': [{
+                        'type': int,
+                        'content: string
+                    }]
+                }
+        """
         req = apis_pb2.HashReq(hash=receipt_hash)
         res = self._stub.GetTxReceiptByHash(req)
         return (protobuf_to_dict(res.txReceiptRaw), res.hash)
@@ -119,7 +202,34 @@ class IOST():
     # 	tx.TxReceiptRaw txReceiptRaw = 1;
     # 	bytes hash = 2;
     # }
-    def get_tx_receipt_by_tx_hash(self, tx_hash):
+    def get_tx_receipt_by_tx_hash(self, tx_hash: str) -> (dict, bytes):
+        """
+        Gets a transaction receipt by its transaction hash value.
+
+        Note:
+            REST API: "/getTxReceiptByTxHash/{hash}"
+
+        Args:
+            receipt_hash (str): The base58 hash string of the transaction.
+
+        Returns:
+            (dict, bytes): A tuple containing the receipt content as a dict
+            and its hash as bytes. The dict has the following format::
+
+                {
+                    'txHash': bytes,
+                    'gasUsage': int,
+                    'status': {
+                        'code': int,
+                        'message': string
+                    },
+                    'succActionNum': int,
+                    'receipts': [{
+                        'type': int,
+                        'content: string
+                    }]
+                }
+        """
         req = apis_pb2.HashReq(hash=tx_hash)
         res = self._stub.GetTxReceiptByTxHash(req)
         return (protobuf_to_dict(res.txReceiptRaw), res.hash)
@@ -160,7 +270,59 @@ class IOST():
     #     repeated bytes txs = 3;
     #     repeated bytes receipts = 4;
     # }
-    def get_block_by_hash(self, block_hash, complete=False):
+    def get_block_by_hash(self, block_hash: str, complete: bool = False) -> dict:
+        """
+        Gets a block by its hash.
+
+        Note:
+            REST API: "/getBlockByHash/{hash}/{complete}"
+
+        Args:
+            block_hash: The base58 hash string of the block.
+            complete: If True, returns the whole block, otherwise
+                returns the head and the list of transaction and receipt hashes.
+
+        Returns:
+            dict: The whole block if `complete` is True,
+                otherwise the head and the list of transactions and receipts.
+
+            When `complete` is False, the dict has the following format::
+
+                {
+                    'head': {
+                        #'version': int,
+                        #'parentHash': bytes,
+                        'txsHash': bytes,
+                        'merkleHash': bytes,
+                        #'info': bytes,
+                        #'number': int,
+                        'witness': string,
+                        #'time': int
+                    },
+                    'hash': bytes,
+                    'txhash': [ bytes ],
+                    'receiptHash': [ bytes ]
+                }
+
+            When `complete` is True, the dict has the following format::
+
+                {
+                    'head': {
+                        #'version': int,
+                        #'parentHash': bytes,
+                        'txsHash': bytes,
+                        'merkleHash': bytes,
+                        #'info': bytes,
+                        #'number': int,
+                        'witness': string,
+                        #'time': int
+                    },
+                    'hash': bytes,
+                    'txs': [ TxRaw (see return type of ``get_tx_by_hash``) ],
+                    'receipts': [ TxReceiptRaw (see return type of ``get_tx_receipt_by_hash``) ]
+                }
+
+        """
         req = apis_pb2.BlockByHashReq(hash=block_hash, complete=complete)
         res = self._stub.GetBlockByHash(req)
         return protobuf_to_dict(res)
@@ -184,7 +346,67 @@ class IOST():
     # 	repeated tx.TxReceiptRaw receipts = 5;
     # 	repeated bytes receiptHash = 6;
     # }
-    def get_block_by_num(self, block_num, complete=False):
+    # message BlockHead {
+    #     int64 version = 1;
+    #     bytes parentHash = 2;
+    #     bytes txsHash = 3;
+    #     bytes merkleHash = 4;
+    #     bytes info = 5;
+    #     int64 number = 6;
+    #     string witness = 7;
+    #     int64 time = 8;
+    # }
+    def get_block_by_num(self, block_num: int, complete: bool = False) -> dict:
+        """
+        Gets a block by its number.
+
+        Note:
+            REST API: "/getBlockByNum/{num}/{complete}"
+
+        Args:
+            block_num: The number of the block.
+            complete: If True, returns the whole block,
+                otherwise returns the head and the list of transaction hashes.
+
+        Returns:
+            dict: The whole block if `complete` is True,
+                otherwise the head and the list of transactions.
+
+            When `complete` is False, the dict has the following format::
+
+                {
+                    'head': {
+                        #'version': int,
+                        #'parentHash': bytes,
+                        'txsHash': bytes,
+                        'merkleHash': bytes,
+                        #'info': bytes,
+                        #'number': int,
+                        'witness': string,
+                        #'time': int
+                    },
+                    'hash': bytes,
+                    'txhash': [ bytes ]
+                }
+
+            When `complete` is True, the dict has the following format::
+
+                {
+                    'head': {
+                        #'version': int,
+                        #'parentHash': bytes,
+                        'txsHash': bytes,
+                        'merkleHash': bytes,
+                        #'info': bytes,
+                        #'number': int,
+                        'witness': string,
+                        #'time': int
+                    },
+                    'hash': bytes,
+                    'txs': [ TxRaw (see return type of ``get_tx_by_hash``) ]
+                }
+
+        """
         req = apis_pb2.BlockByNumReq(num=block_num, complete=complete)
         res = self._stub.getBlockByNum(req)
         return protobuf_to_dict(res)
@@ -204,7 +426,21 @@ class IOST():
     # 	// the queried balance
     # 	int64 balance=1;
     # }
-    def get_balance(self, account_id, use_longest_chain=False):
+    def get_balance(self, account_id: str, use_longest_chain: bool = True) -> int:
+        """
+        Gets the balance of an account by its id.
+
+        Note:
+            REST API: "/getBalance/{ID}/{useLongestChain}"
+
+        Args:
+            account_id (str): The ID of the account.
+            use_longest_chain (bool): If True, also gets balance from pending blocks
+                (in the longest chain)
+
+        Returns:
+            int: the balance of the account (units?).
+        """
         req = apis_pb2.GetBalanceReq(ID=account_id, useLongestChain=use_longest_chain)
         res = self._stub.GetBalance(req)
         return res.balance
@@ -218,7 +454,16 @@ class IOST():
     # message GetNetIDRes {
     # 	string ID=1;
     # }
-    def get_net_id(self):
+    def get_net_id(self) -> str:
+        """
+        Gets the ID of the node.
+
+        Note:
+            REST API: "/getNetID"
+
+        Returns:
+            The ID of the node as a base58 hash string.
+        """
         res = self._stub.GetNetID(Empty())
         return res.ID
 
@@ -236,7 +481,20 @@ class IOST():
     # message GetStateRes {
     # 	string value=1;
     # }
-    def get_state(self, key, field=None):
+    def get_state(self, key: str, field: str = None) -> str:
+        """
+        Gets the value of a key in the StateDB.
+
+        Note:
+            REST API: "/getState/{key}"
+
+        Args:
+            key (str): The key.
+            field (str): Required if `key` is a map.
+
+        Returns:
+            str: StateDB[`key`] or StateDB[`key`][`field`] if `key` is a map.
+        """
         req = apis_pb2.GetStateReq(key=key, field=field)
         res = self._stub.GetState(req)
         return res.value
@@ -257,7 +515,7 @@ class IOST():
     # 	string hash=1;
     # }
     # TODO: need to replace data with a tx.TxRaw? (or take raw_tx a dict and convert to proto)
-    def send_raw_tx(self, raw_tx):
+    def send_raw_tx(self, raw_tx: bytes) -> str:
         req = apis_pb2.RawTxReq(data=raw_tx)
         res = self._stub.SendRawTx(req)
         return res.hash
@@ -277,7 +535,7 @@ class IOST():
     # 	uint64 gas=1;
     # }
     # TODO: need to replace data with a tx.TxRaw?
-    def estimate_gas(self, raw_tx):
+    def estimate_gas(self, raw_tx: bytes) -> int:
         req = apis_pb2.RawTxReq(data=raw_tx)
         res = self._stub.EstimateGas(req)
         return res.gas
@@ -309,7 +567,7 @@ class IOST():
     # TODO: event_topic is a list of enum (need to pass an iterator?)
     # TODO: if topics is a scalar, transform it into a 1 element list
     # TODO: check that each topic is a valid Enum value
-    def subscribe(self, topics):
+    def subscribe(self, topics: [int]) -> dict:
         print(ptd.get_field_names_and_options(event_pb2.Event))
         req = apis_pb2.SubscribeReq(topics=topics)
         res = self._stub.Subscribe(req)
