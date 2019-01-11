f6be430aee865bf16e70b417dc016965d63d1c65
Cyril Clement <dossiman@gmail.com>
2019-01-11 21:44:27 +0700

    fix protobuf-to-dict dependency

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 setup(
   name = 'pyost',         # How you named your package folder (MyLib)
   packages = ['pyost','pyost.rpc.pb'],   # Chose the same as "name"
-  version = 'v2.1-beta.3',      # Start with a small number and increase it with every change you make
+  version = 'v2.1-beta.4',      # Start with a small number and increase it with every change you make
   license='LGPG',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
   description = 'Python SDK for the IOST Blockchain',   # Give a short description about your library
   author = 'Cyril Clement',                   # Type in your name
   author_email = 'dossiman@domain.com',      # Type in your E-Mail
   url = 'https://github.com/dossiman/pyost',   # Provide either the link to your github or to your website
-  download_url = 'https://github.com/dossiman/pyost/archive/v2.1-beta.3.tar.gz',    # I explain this later on
+  download_url = 'https://github.com/dossiman/pyost/archive/v2.1-beta.4.tar.gz',    # I explain this later on
   keywords = ['IOST', 'blockchain', 'API', 'SDK'],   # Keywords that define your package best
   install_requires=[            # I get to this in a second
     'base58',
@@ -16,7 +16,7 @@ setup(
     'ed25519',
     'grpcio',
     'googleapis-common-protos',
-    'protobuf-to-dict'],
+    'protobuf3-to-dict'],
   classifiers=[
     'Development Status :: 4 - Beta',      # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state of your package
     'Intended Audience :: Developers',      # Define that your audience are developers
